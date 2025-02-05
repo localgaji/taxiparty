e53a5ae6064fbd401f1ca944f97ac8744eb5a3f9
localgaji <oorongcha@naver.com>
2025-02-05 22:23:43 +0900

    chore: swagger configuration

---
+++ src/main/java/com/localgaji/taxi/__global__/docs/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.localgaji.taxi.__global__.docs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("TAXI API")
+                        .description("API 명세서")
+                        .version("v1.0.0"));
+    }
+}
