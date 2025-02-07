1f95fd5ee283fabb2c1128ee9908cbddf33938df
localgaji <oorongcha@naver.com>
2025-02-07 17:34:45 +0900

    feat: 다른 user 정보 조회 api

--- src/main/java/com/localgaji/taxi/user/UserController.java
+++ src/main/java/com/localgaji/taxi/user/UserController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,21 @@ import static com.localgaji.taxi.user.user_dto.ResponseUser.*;
 @Tag(name = "회원 정보", description = "회원 정보 조회/수정 관련 API")
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping
     @Operation(summary = "내 정보 조회")
     public ResponseEntity<Response<GetMyInfoResponse>> getMyInfo(@AuthUser User user) {
         GetMyInfoResponse responseBody = new GetMyInfoResponse(user);
+        return ResponseEntity.ok().body(success(responseBody));
+    }
+
+    @GetMapping("/{userId}")
+    @Operation(summary = "다른 회원 정보 조회")
+    public ResponseEntity<Response<GetUserInfoResponse>> getUserInfo(@PathVariable Long userId) {
+        User user = userService.findUserById(userId);
+        GetUserInfoResponse responseBody = new GetUserInfoResponse(user);
         return ResponseEntity.ok().body(success(responseBody));
     }
 }
