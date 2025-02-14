89483a64ce7d5d62d4bc3d7a9193c18593717792
localgaji <oorongcha@naver.com>
2025-02-15 06:38:30 +0900

    feat: 계좌 등록 api 추가

--- src/main/java/com/localgaji/taxi/user/UserController.java
+++ src/main/java/com/localgaji/taxi/user/UserController.java
@@ -2,15 +2,14 @@ package com.localgaji.taxi.user;
 
 import com.localgaji.taxi.__global__.auth_global.AuthUser;
 import com.localgaji.taxi.__global__.utils.ApiUtil.Response;
+import com.localgaji.taxi.account.Account;
+import com.localgaji.taxi.account.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.localgaji.taxi.__global__.utils.ApiUtil.*;
 import static com.localgaji.taxi.user.user_dto.ResponseUser.*;
@@ -22,6 +21,7 @@ import static com.localgaji.taxi.user.user_dto.ResponseUser.*;
 public class UserController {
 
     private final UserService userService;
+    private final AccountService accountService;
 
     @GetMapping
     @Operation(summary = "내 정보 조회")
@@ -36,5 +36,13 @@ public class UserController {
         User user = userService.findUserById(userId);
         GetUserInfoResponse responseBody = new GetUserInfoResponse(user);
         return ResponseEntity.ok().body(success(responseBody));
+    }
+
+    @PatchMapping("/account")
+    @Operation(summary = "계좌 등록")
+    public ResponseEntity<Response<String>> patchAccount(@AuthUser User user,
+                                                         @RequestBody Account body) {
+        accountService.addAccount(user, body);
+        return ResponseEntity.ok().body(success(null));
     }
 }
