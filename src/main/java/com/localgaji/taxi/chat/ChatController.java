385273a29eff57fa15dfdb7bf8f43b62f5228a51
localgaji <oorongcha@naver.com>
2025-02-24 22:52:14 +0900

    feat: controller

---
+++ src/main/java/com/localgaji/taxi/chat/ChatController.java
@@ -0,0 +1,27 @@
+package com.localgaji.taxi.chat;
+
+import com.localgaji.taxi.__global__.auth_global.AuthUser;
+import com.localgaji.taxi.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static com.localgaji.taxi.__global__.utils.ApiUtil.*;
+import static com.localgaji.taxi.chat.dto.ResponseChat.*;
+
+@RequiredArgsConstructor
+public class ChatController {
+    private final ChatService chatService;
+
+    @GetMapping("/party/{party_id}/chatList")
+    public ResponseEntity<Response<GetChatListRes>> getChatList(@AuthUser User user,
+                                                                @PathVariable Long partyId,
+                                                                @RequestParam(required = false) Long cursor,
+                                                                @RequestParam(defaultValue = "20") int amount) {
+        GetChatListRes responseBody = chatService.getChatList(user, partyId, amount, cursor);
+        return ResponseEntity.ok().body(success(responseBody));
+    }
+
+}
