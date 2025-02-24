512fdf08ade70cb32bcb0c564ab972d711a22c14
localgaji <oorongcha@naver.com>
2025-02-24 23:27:11 +0900

    feat: websocket config

---
+++ src/main/java/com/localgaji/taxi/__global__/WebSocketConfig.java
@@ -0,0 +1,38 @@
+package com.localgaji.taxi.__global__;
+
+import com.localgaji.taxi.__global__.auth_global.StompAuthChannelInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker @RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 발신 (발행)
+        registry.setApplicationDestinationPrefixes("/pub");
+
+        // 수신 (구독)
+        registry.enableSimpleBroker("/sub");
+    }
+
+
+    private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompAuthChannelInterceptor);
+    }
+}
